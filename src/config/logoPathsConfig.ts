bd0cebc922a3f881418ae995eb5239e3893726a7
Eric Faraut <eric.faraut@hotmail.fr>
2026-01-06 17:00:48 +0100

    vitest is set up.

---
+++ src/config/logoPathsConfig.ts
@@ -0,0 +1,35 @@
+import type { LogoKey } from "@/types/LogoKey";
+
+export const basePath = import.meta.env.BASE_URL || '/';
+export const logosPaths = 'logos/';
+const fullPath = basePath + logosPaths;
+
+export const logos: Record<LogoKey, string> = {
+    HTML: fullPath + 'HTML5.webp',
+    CSS: fullPath + 'CSS3.webp',
+    SCSS: fullPath + 'SCSS.webp',
+    tailwindCSS : fullPath + 'tailwindCSS.webp',
+    flowbiteReact : fullPath + 'flowbite-react.webp', 
+    vite: fullPath + 'vite.webp',
+    JS: fullPath + 'JS.webp',
+    TS: fullPath + 'TS.webp',
+    jest: fullPath + 'jest.webp',
+    react: fullPath + 'react.webp',
+    reactRouter: fullPath + 'react-router.webp',
+    redux: fullPath + 'redux.webp',
+    lighthouse: fullPath + 'lighthouse.webp',
+    GTmetrix: fullPath + 'GTmetrix.webp',
+    WAvE: fullPath + 'WAvE.webp',
+    axeDevTool: fullPath + 'axeDevTool.webp',
+    greenITanalysis: fullPath + 'greenITanalysis.webp',
+    htmlValidate: fullPath + 'html-validate.webp',
+    stylelint: fullPath + 'stylelint.webp',
+    pa11y: fullPath + 'pa11y.webp',
+    vitestAxe: fullPath + 'vitest-axe.webp',
+    linkedIn: fullPath + 'linkedIn.webp',
+    gitHub: fullPath + 'gitHub.webp',
+    E: fullPath + 'logo.webp',
+    feedly: fullPath + 'feedly.webp',
+    notion: fullPath + 'notion.webp',
+    excalidraw: fullPath + 'excalidraw.webp'
+}
---
+++ src/types/LogoKey.ts
@@ -0,0 +1,36 @@
+/**
+ * // This is a less mutable reusable static type.
+ * // A recommended practice for dynamic keys should be
+ * 
+ * import {logos} from '../config/logosPaths'
+ * 
+ * type LogoKey = keyof typeof logos;
+ */
+export type LogoKey = 
+    'HTML' |
+    'CSS' |
+    'SCSS' |
+    'tailwindCSS' |
+    'flowbiteReact' |
+    'vite' |
+    'JS' |
+    'TS' |
+    'jest' |
+    'react' |
+    'reactRouter' |
+    'redux' |
+    'lighthouse' |
+    'GTmetrix' |
+    'WAvE' |
+    'axeDevTool' |
+    'greenITanalysis' |
+    'htmlValidate' |
+    'stylelint' |
+    'pa11y' |
+    'vitestAxe' |
+    'linkedIn' |
+    'gitHub' |
+    'E' |
+    'feedly' |
+    'notion' |
+    'excalidraw'
