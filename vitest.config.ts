a06b65a7e36723be50f55411fb89235158e5888c
Eric Faraut <eric.faraut@hotmail.fr>
2026-03-01 18:04:53 +0100

    Vitest config is cleaner.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,24 +1,21 @@
 import 'vitest/config';
 
 import { getViteConfig } from 'astro/config';
-//import { defineConfig } from 'vitest/config';
 
 import react from '@vitejs/plugin-react';
 
-//export default defineConfig({
 export default getViteConfig({
     plugins: [react()],
     test: {
         globals: true,
         setupFiles: './vitest-setup.ts',
         include:[
-        'test/accessibility/**/*.pa11y.test.ts',
-        'test/accessibility/unit/**/*.a11y.test.tsx'
+        'test/accessibility/**/*.pa11y.test.ts'
         ],
         exclude:[
             'test/integration/**/*.integration.test.ts'
         ],
-        /* implicit default */
+        /* default */
         environment: 'node'
         }
 });
