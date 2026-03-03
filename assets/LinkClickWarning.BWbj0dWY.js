337e6d9537684cf3e6b2140f56a45f75279b1042
Eric Faraut <eric.faraut@hotmail.fr>
2026-03-03 19:26:12 +0100

    Updates

--- assets/LinkClickWarning.BWbj0dWY.js
+++
@@ -1,1 +0,0 @@
-import{L as f}from"./LinkClickWarning.Bq4xLCul.js";export{f as default};
---
+++ assets/LinkClickWarning.DYMGdwU1.js
@@ -0,0 +1,30 @@
+var o={exports:{}},r={};var s;function d(){if(s)return r;s=1;var x=Symbol.for("react.transitional.element"),p=Symbol.for("react.fragment");function u(l,n,t){var e=null;if(t!==void 0&&(e=""+t),n.key!==void 0&&(e=""+n.key),"key"in n){t={};for(var i in n)i!=="key"&&(t[i]=n[i])}else t=n;return n=t.ref,{$$typeof:x,type:l,key:e,ref:n!==void 0?n:null,props:t}}return r.Fragment=p,r.jsx=u,r.jsxs=u,r}var a;function v(){return a||(a=1,o.exports=d()),o.exports}var c=v();function R(){return c.jsx("span",{className:`
+        absolute 
+        bottom-full 
+        mb-2 
+        inset-x-0 
+        mx-auto 
+        z-40 
+
+        bg-white 
+        text-gray-900 
+        text-xs 
+        rounded 
+        py-1 
+        px-3 
+
+        whitespace-nowrap
+        max-w-[90vw]
+        overflow-hidden
+        text-ellipsis
+
+        opacity-0 
+        pointer-events-none 
+        group-hover:opacity-100 
+        group-focus-within:opacity-100 
+
+        transition-opacity 
+        duration-600 
+        ease-in-out
+        
+        motion-reduce:transition-none`,children:"🔒 Vérifiez toujours l'URL avant de cliquer!"})}export{R as default};
