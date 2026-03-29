7e07e421924c614e0e2c9ace0028b2a2cc922d80
Eric Faraut <eric.faraut@hotmail.fr>
2026-03-29 16:24:21 +0200

    Tailwind is updated to 4.

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
-
 export default defineConfig({
   site: 'https://ericdev1o.github.io',
   base: '/OC900_p12_portfolio_astro/',
-
-  integrations: [
-    tailwind()
-  ],
 
   build: {
     assets: 'assets'
