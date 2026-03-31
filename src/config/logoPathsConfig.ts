394d32824ec5d623a15fb89de63c20f88f0d9529
Eric Faraut <eric.faraut@hotmail.fr>
2026-03-31 11:21:40 +0200

    Astro <Image> replaces <img>: no srcSet, stable LCP & CLS.

--- src/config/logoPathsConfig.ts
+++ src/config/logoPathsConfig.ts
@@ -1,38 +1,17 @@
-export const basePath = import.meta.env.BASE_URL || '/';
-const fullPath = `${basePath}logos/`;
+const modules = import.meta.glob<{ default: ImageMetadata }>(
+    '@/assets/images/logos/*.webp', 
+    { eager: true }
+);
 
-export const logosRaw = {
-    HTML: fullPath + 'HTML5.webp',
-    CSS: fullPath + 'CSS3.webp',
-    SCSS: fullPath + 'SCSS.webp',
-    tailwindCSS : fullPath + 'tailwindCSS.webp',
-    vite: fullPath + 'vite.webp',
-    JS: fullPath + 'JS.webp',
-    TS: fullPath + 'TS.webp',
-    jest: fullPath + 'jest.webp',
-    react: fullPath + 'react.webp',
-    reactRouter: fullPath + 'reactRouter.webp',
-    redux: fullPath + 'redux.webp',
-    lighthouse: fullPath + 'lighthouse.webp',
-    GTmetrix: fullPath + 'GTmetrix.webp',
-    WAvE: fullPath + 'WAvE.webp',
-    axeDevTool: fullPath + 'axeDevTool.webp',
-    greenITanalysis: fullPath + 'greenITanalysis.webp',
-    stylelint: fullPath + 'stylelint.webp',
-    playwright: fullPath + 'playwright.webp',
-    linkedIn: fullPath + 'linkedIn.webp',
-    linkedInRetina: fullPath + 'linkedIn_w90px.webp',
-    gitHub: fullPath + 'gitHub.webp',
-    gitHubRetina: fullPath + 'gitHub_w78px.webp',
-    E: fullPath + 'logo.webp',
-    Eretina: fullPath + 'logo_w23px.webp',
-    feedly: fullPath + 'feedly.webp',
-    notion: fullPath + 'notion.webp',
-    excalidraw: fullPath + 'excalidraw.webp'
-} as const;
+export const logosMap = Object.fromEntries(
+    Object.entries(modules).map(([path, module]) => {
+        const filename = path.split('/').pop();
+        if( ! filename)
+            throw new Error(`Invalid path: ${path}`);
+        const key = filename.replace('.webp', '');
 
-export type LogoKey = keyof typeof logosRaw;
+        return [key, module.default];
+    })
+) as Record<string, ImageMetadata>;
 
-export function getLogoURI(key: LogoKey): string {
-    return logosRaw[key];
-}
+export type LogoKey = keyof typeof logosMap;
